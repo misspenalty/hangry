4d0089035d0f278a02e9f5b117627cfe93d59e7e
misspenalty <saara.hukka@gmail.com>
2018-07-23 21:13:36 +0200

    remove code that was commented out

--- src/main/java/org/misspenalty/hangry/web/Application.java
+++ src/main/java/org/misspenalty/hangry/web/Application.java
@@ -1,30 +1,15 @@
 package org.misspenalty.hangry.web;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
 
 @SpringBootApplication
 @EnableJpaRepositories("org.misspenalty")
-public class Application extends SpringBootServletInitializer  {
+public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-//	
-//	@Bean
-//	public CommandLineRunner demo(DishDao repository) {
-//		return (args) -> {
-//			for (Dish customer : repository.findAll()) {
-//				System.out.println(customer);
-//			}
-//		
-//		};
-//	}
 }
