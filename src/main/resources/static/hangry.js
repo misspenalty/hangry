4f07ac828165b4fdac9754176ca1b30cc93b83dd
misspenalty <saara.hukka@gmail.com>
2018-07-26 19:00:10 +0200

    center warning message, format code

--- src/main/resources/static/hangry.js
+++ src/main/resources/static/hangry.js
@@ -3,30 +3,33 @@ $('#search')
         function(event) {
             event.preventDefault();
             var query = $("#dish").val();
-            
+
             $.getJSON("/hangry/restaurant?dish=" + query, function(json) {
-            	 var items = [];
-                 $
-                     .each(
-                         json,
-                         function(key,
-                             val) {
-                             items
-                                 .push("<div class=\"row justify-content-center\">" +
-                                     "<div class=\"card card-body bg-light\">" +
-                                     "<a href=" + val['restaurant']['restaurantPage'] + ">" +
-                                     val['restaurant']['name'] +
-                                     "</a>" +
-                                     val['name'] +
-                                     "</div>" +
-                                     "</div>");
-                         });
-                 $("#result").html(items.join(""));
-                 $("#searchresults").html("<strong>" + items.length + "</strong> results found.");
-            	})
-            	  .fail(function(jqXHR, textStatus, errorThrown) {
-            		  if (jqXHR.status = 400) {
-            			  $("#searchresults").html(JSON.parse(jqXHR.responseText)['message']);
-            		  }	  
-            	  })
+                    var items = [];
+                    $
+                        .each(
+                            json,
+                            function(key,
+                                val) {
+                                items
+                                    .push("<div class=\"row justify-content-center\">" +
+                                        "<div class=\"card card-body bg-light\">" +
+                                        "<a href=" + val['restaurant']['restaurantPage'] + ">" +
+                                        val['restaurant']['name'] +
+                                        "</a>" +
+                                        val['name'] +
+                                        "</div>" +
+                                        "</div>");
+                            });
+                    $("#result").html(items.join(""));
+                    $("#searchresults").html("<strong>" + items.length + "</strong> results found.");
+                })
+                .fail(function(jqXHR, textStatus, errorThrown) {
+                    if (jqXHR.status = 400) {
+                        $("#result").html("");
+                        $("#searchresults").html("<div class=\"row justify-content-center\">" +
+                            JSON.parse(jqXHR.responseText)['message'] +
+                            "</div>");
+                    }
+                })
         });
