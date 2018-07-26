8af1a0904f1641b8e4e549a012a1f2498861f2e5
misspenalty <saara.hukka@gmail.com>
2018-07-26 16:17:56 +0200

    add exception handling if search query contains less than 3 characters

--- src/main/java/org/misspenalty/hangry/web/RestaurantController.java
+++ src/main/java/org/misspenalty/hangry/web/RestaurantController.java
@@ -1,9 +1,14 @@
 package org.misspenalty.hangry.web;
 
+import java.io.IOException;
 import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,7 +24,15 @@ public class RestaurantController {
 
 	@RequestMapping(value = "/restaurant", method = RequestMethod.GET)
 	public @ResponseBody List<Dish> restaurant(@RequestParam(value = "dish") String dish) {
+		if (dish.length() <= 2) {
+			throw new IllegalArgumentException();
+		}
 		return dishDao.findByNameContainingOrderByRestaurantNameAscNameAsc(dish);
+	}
+	
+	@ExceptionHandler(IllegalArgumentException.class)
+	void handleBadRequests(HttpServletResponse response) throws IOException {
+		response.sendError(HttpStatus.BAD_REQUEST.value(), "Search query must contain at least 3 characters.");
 	}
 
 }
--- src/main/resources/static/hangry.js
+++ src/main/resources/static/hangry.js
@@ -3,28 +3,30 @@ $('#search')
         function(event) {
             event.preventDefault();
             var query = $("#dish").val();
-            $
-                .getJSON(
-                    "/hangry/restaurant?dish=" + query,
-                    function(data) {
-                        var items = [];
-                        $
-                            .each(
-                                data,
-                                function(key,
-                                    val) {
-                                    items
-                                        .push("<div class=\"row justify-content-center\">" +
-                                            "<div class=\"card card-body bg-light\">" +
-                                            "<a href=" + val['restaurant']['restaurantPage'] + ">" +
-                                            val['restaurant']['name'] +
-                                            "</a>" +
-                                            val['name'] +
-                                            "</div>" +
-                                            "</div>");
-                                });
-                        $("#result").html(items.join(""));
-                        $("#searchresults").html("<strong>" + items.length + "</strong> results found.");
-
-                    });
+            
+            $.getJSON("/hangry/restaurant?dish=" + query, function(json) {
+            	 var items = [];
+                 $
+                     .each(
+                         json,
+                         function(key,
+                             val) {
+                             items
+                                 .push("<div class=\"row justify-content-center\">" +
+                                     "<div class=\"card card-body bg-light\">" +
+                                     "<a href=" + val['restaurant']['restaurantPage'] + ">" +
+                                     val['restaurant']['name'] +
+                                     "</a>" +
+                                     val['name'] +
+                                     "</div>" +
+                                     "</div>");
+                         });
+                 $("#result").html(items.join(""));
+                 $("#searchresults").html("<strong>" + items.length + "</strong> results found.");
+            	})
+            	  .fail(function(jqXHR, textStatus, errorThrown) {
+            		  if (jqXHR.status = 400) {
+            			  $("#searchresults").html(JSON.parse(jqXHR.responseText)['message']);
+            		  }	  
+            	  })
         });
